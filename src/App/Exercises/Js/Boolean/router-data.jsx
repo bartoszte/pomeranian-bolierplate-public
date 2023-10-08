4ba4df39af31385f3cc25a44f3387b672f6f01e8
bartoszte <bart.tarnopolski@gmail.com>
2023-10-08 16:54:09 +0200

    booleans numbers operators

---
+++ src/App/Exercises/Js/Boolean/router-data.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Boolean } from './index';
+
+export const booleanMetaData = {
+  path: 'boolean-exercise',
+
+  date: '08-10-2023',
+
+  linkLabel: 'Ćwiczenia związane z boolean',
+
+  blockNo: 18,
+
+  element: <Boolean />,
+
+  tags: [
+    'true',
+
+    'false',
+
+    'boolean',
+
+    'operators',
+
+    'operands',
+
+    '&&',
+
+    '||',
+
+    '!',
+
+    '===',
+
+    '!==',
+
+    '==',
+
+    '!=',
+
+    '>',
+
+    '<',
+
+    '>=',
+
+    '<=',
+  ],
+};

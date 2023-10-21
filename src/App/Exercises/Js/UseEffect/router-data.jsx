fab6d3c3c18b266eaf417401acbe6fe08767c025
bartoszte <bart.tarnopolski@gmail.com>
2023-10-21 17:45:49 +0200

    timeout and use effects and sliding banner draft

---
+++ src/App/Exercises/Js/UseEffect/router-data.jsx
@@ -0,0 +1,15 @@
+import { JsUseEffect } from '.';
+
+export const JsUseEffectMetaData = {
+  path: 'use-effect',
+
+  date: '21-10-2023',
+
+  linkLabel: 'Ćwiczenie związane z useEffect',
+
+  blockNo: 21,
+
+  element: <JsUseEffect />,
+
+  tags: ['useEffect', 'react'],
+};
